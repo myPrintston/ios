cbe702b0ce3cea57ca9736dc673ed428a23b356a
Michael J Kim <mjkim1993@gmail.com>
2014-03-28 21:00:15 -0400

    Create Printer Class

---
+++ myPrintston/MPSPrinter.h
@@ -0,0 +1,14 @@
+//
+//  MPSPrinter.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 3/28/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPSPrinter : NSObject {
+    int count;
+}
+@end
---
+++ myPrintston/MPSPrinter.m
@@ -0,0 +1,15 @@
+//
+//  MPSPrinter.m
+//  myPrintston
+//
+//  Created by Michael J Kim on 3/28/14.
+//
+//
+
+#import "MPSPrinter.h"
+
+@implementation MPSPrinter
+
+
+
+@end
