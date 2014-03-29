985c7789c1818f449a76c5c8341411ec583a8a60
Michael J Kim <mjkim1993@gmail.com>
2014-03-28 21:00:53 -0400

    Created TableViewController

---
+++ myPrintston/MPSTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPSTableViewController.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 3/28/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSTableViewController : UITableViewController
+
+@end
