5041e8721d0d32d7726cec841e3335a52af2cda6
Michael J Kim <mjkim1993@gmail.com>
2014-04-03 11:55:43 -0400

    create View Controller for Printer Details

---
+++ myPrintston/MPSPrinterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPSPrinterViewController.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSPrinterViewController : UIViewController
+
+@end
