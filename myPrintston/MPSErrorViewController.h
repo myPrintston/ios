dd6a675b28cff44bfef1c7350be280b9b3a330e8
Michael J Kim <mjkim1993@gmail.com>
2014-04-06 14:05:29 -0400

    Create Error Checklist View Controller

---
+++ myPrintston/MPSErrorViewController.h
@@ -0,0 +1,16 @@
+//
+//  MPSErrorViewController.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSErrorViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITableView *errorList;
+@property (weak, nonatomic) IBOutlet UIButton *submitButton;
+
+@end
