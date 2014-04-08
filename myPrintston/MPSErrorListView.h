c6c07301ba2eb3243e380d2920b0dc2ffdad4276
Michael J Kim <mjkim1993@gmail.com>
2014-04-08 13:39:50 -0400

    Create TableView for Error Checklist

---
+++ myPrintston/MPSErrorListView.h
@@ -0,0 +1,13 @@
+//
+//  MPSErrorListView.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSErrorListView : UITableView
+
+@end
---
+++ myPrintston/MPSErrorListView.m
@@ -0,0 +1,31 @@
+//
+//  MPSErrorListView.m
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/6/14.
+//
+//
+
+#import "MPSErrorListView.h"
+
+@implementation MPSErrorListView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
