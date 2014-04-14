c0153ea82b274515448f169b19e6b7d2ac021245
Michael J Kim <mjkim1993@gmail.com>
2014-04-14 00:25:00 -0400

    Create MapAnnotation objects

---
+++ myPrintston/MPSMapAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  MPSMapAnnotation.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/13/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPSMapAnnotation : NSObject
+
+@end
---
+++ myPrintston/MPSMapAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  MPSMapAnnotation.m
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/13/14.
+//
+//
+
+#import "MPSMapAnnotation.h"
+
+@implementation MPSMapAnnotation
+
+@end
