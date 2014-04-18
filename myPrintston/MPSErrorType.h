b1d108d20ae0c8ba820c0ea003e349acc684e4df
Michael J Kim <mjkim1993@gmail.com>
2014-04-17 20:09:39 -0400

    Create ErrorType object

---
+++ myPrintston/MPSErrorType.h
@@ -0,0 +1,13 @@
+//
+//  MPSErrorType.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/17/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPSErrorType : NSObject
+
+@end
---
+++ myPrintston/MPSErrorType.m
@@ -0,0 +1,13 @@
+//
+//  MPSErrorType.m
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/17/14.
+//
+//
+
+#import "MPSErrorType.h"
+
+@implementation MPSErrorType
+
+@end
