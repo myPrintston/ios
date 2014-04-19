c4f4f7bbdcd143afad0399817d5e24d029b27d32
Michael J Kim <mjkim1993@gmail.com>
2014-04-19 01:34:01 -0400

    Add custom TabBarController to globalize printers

---
+++ myPrintston/MPSTabController.h
@@ -0,0 +1,13 @@
+//
+//  MPSTabController.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/19/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSTabController : UITabBarController
+
+@end
