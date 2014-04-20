368ebd74614eff56a86298122140ec6d756fc902
Michael J Kim <mjkim1993@gmail.com>
2014-04-20 14:23:20 -0400

    add MapNavController class

---
+++ myPrintston/MPSMapNavController.h
@@ -0,0 +1,13 @@
+//
+//  MPSMapNavController.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/20/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSMapNavController : UINavigationController
+
+@end
