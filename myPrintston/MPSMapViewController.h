bf3c42ae6a0058bed8567592cc1a3ce89cca9d1f
Michael J Kim <mjkim1993@gmail.com>
2014-04-20 14:26:45 -0400

    Create MapViewController class

---
+++ myPrintston/MPSMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPSMapViewController.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 4/20/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSMapViewController : UIViewController
+
+@end
