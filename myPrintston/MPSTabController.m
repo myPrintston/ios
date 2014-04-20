cba668a65d1a381b554dd343b3b8372383426fca
Michael J Kim <mjkim1993@gmail.com>
2014-04-20 14:54:08 -0400

    remove commented NSLog calls

--- myPrintston/MPSTabController.m
+++ myPrintston/MPSTabController.m
@@ -110,7 +110,6 @@
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
 {
-    //NSLog(@"didUpdateToLocation: %@", newLocation);
     CLLocation *currentLocation = newLocation;
     
     if (currentLocation != nil) {
@@ -120,8 +119,6 @@
     
     [MPSPrinter setUserLongitude: -74.6551];
     [MPSPrinter setUserLatitude:   40.3450];
-    
-    //NSLog(@"Location Update");
 }
 
 /*
