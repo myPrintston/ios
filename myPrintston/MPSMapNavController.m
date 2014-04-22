25d96fd47375c118ccbfa373a06b1883ddab652d
Michael J Kim <mjkim1993@gmail.com>
2014-04-22 01:18:46 -0400

    pass printers to MPSMapViewController

--- myPrintston/MPSMapNavController.m
+++ myPrintston/MPSMapNavController.m
@@ -39,7 +39,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -50,6 +50,6 @@
     MPSMapViewController *controller = segue.destinationViewController;
     controller.printers = self.printers;
 }
-*/
+
 
 @end
