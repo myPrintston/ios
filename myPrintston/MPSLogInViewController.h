60dc29b55163a4f48a3fee401f0d1bc5aa695300
Michael J Kim <mjkim1993@gmail.com>
2014-05-06 01:43:23 -0400

    add skeleton for LogIn and LogOut

---
+++ myPrintston/MPSLogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPSLogInViewController.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 5/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSLogInViewController : UIViewController
+
+@end
---
+++ myPrintston/MPSLogOutViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPSLogOutViewController.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 5/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSLogOutViewController : UIViewController
+
+@end
