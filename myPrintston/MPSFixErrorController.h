be6e935297244e3ab5aac2225973d7a453258591
Michael J Kim <mjkim1993@gmail.com>
2014-05-07 20:13:05 -0400

    skeleton for FixErrorController

---
+++ myPrintston/MPSFixErrorController.h
@@ -0,0 +1,13 @@
+//
+//  MPSFixErrorController.h
+//  myPrintston
+//
+//  Created by Michael J Kim on 5/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPSFixErrorController : UITableViewController
+
+@end
