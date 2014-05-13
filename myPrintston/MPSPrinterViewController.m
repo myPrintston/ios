e673d3ee4140d969832f80127049bb8c54c85485
Michael J Kim <mjkim1993@gmail.com>
2014-05-13 02:13:32 -0400

    refactor and comment the PrinterViewController

--- myPrintston/MPSPrinterViewController.m
+++ myPrintston/MPSPrinterViewController.m
@@ -22,8 +22,6 @@ extern NSString *IP;
 
 @implementation MPSPrinterViewController
 
-GMSMapView *mapView_;
-
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -37,19 +35,17 @@ GMSMapView *mapView_;
 {
     [super viewDidLoad];
     
-    // setup
+    // Set up the MapView
     self.mapView.myLocationEnabled = YES;
     
-    // get current printer location / add marker
-    double prLat = self.printer.latitude;
-    double prLong = self.printer.longitude;
-    
+    // Initialize the marker for the printer and fill the fields
     GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(prLat, prLong);
-    
+    marker.position = self.printer.location.coordinate;
     marker.title = self.printer.building;
     marker.snippet = self.printer.room;
+    marker.map = self.mapView;
     
+    // Set the color of the marker
     if (self.printer.status == 0)
         marker.icon = [GMSMarker markerImageWithColor:[UIColor greenColor]];
     else if (self.printer.status == 1)
@@ -57,18 +53,17 @@ GMSMapView *mapView_;
     else
         marker.icon = [GMSMarker markerImageWithColor:[UIColor redColor]];
     
-    marker.map = self.mapView;
-    
-    // get map zoom
+    // Get the appropriate map zoom
     CLLocationCoordinate2D myLocation = self.locationManager.location.coordinate;
-    CLLocationCoordinate2D prLocation = CLLocationCoordinate2DMake(prLat, prLong);
+    CLLocationCoordinate2D prLocation = self.printer.location.coordinate;
     GMSCoordinateBounds *bounds = [[GMSCoordinateBounds alloc] initWithCoordinate:myLocation coordinate:prLocation];
 
+    // Zoom in the map
     [self.mapView animateWithCameraUpdate:[GMSCameraUpdate fitBounds:bounds withPadding:20.0f]];
 
     
+    // Display the status of the printer
     self.statusMsg.text = [NSString stringWithFormat:@"Status: %@", self.printer.statusMsg];
-    // Do any additional setup after loading the view.
 }
 
 
@@ -78,25 +73,14 @@ GMSMapView *mapView_;
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - Navigation
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-// Modify the prepareForSegue method by
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([segue.destinationViewController class] == [MPSErrorViewController class]) {
-        MPSErrorViewController *detailController = segue.destinationViewController;
-        detailController.printer = self.printer;
-        detailController.title = [self.printer name];
-    } else {
-        MPSFixErrorController *detailController = segue.destinationViewController;
-        detailController.printer = self.printer;
-        detailController.title = [self.printer name];
-    }
-}
 
+// Method that gets called when the user presses the fix button. Only segues to FixErrorView
+//   if appropriate. When not, tell user/admin why.
 - (IBAction)fix {
     UIAlertView *alert;
     
+    // If the printer has nothing to be fixed, tell the user. It's fine to display this
+    //   message to non-admins
     if (self.printer.status == 0) {
         alert = [[UIAlertView alloc]
                  initWithTitle:@"Error"
@@ -106,6 +90,8 @@ GMSMapView *mapView_;
         return;
     }
     
+    // If user is not an Admin by the internal tracking method, then tell the user to
+    //   log in first.
     if (!isAdmin) {
         alert = [[UIAlertView alloc]
                  initWithTitle:@"Error"
@@ -115,10 +101,12 @@ GMSMapView *mapView_;
         return;
     }
     
+    // Check the login status of the admin in case they timed out.
     NSString *urlstring = [NSString stringWithFormat:@"%@/checklogin", IP];
     NSURL *url = [NSURL URLWithString:urlstring];
     NSData *data = [NSData dataWithContentsOfURL:url];
 
+    // If no response from the server, tell the admin so.
     if (!data) {
         alert = [[UIAlertView alloc]
                  initWithTitle:@"Error"
@@ -130,6 +118,8 @@ GMSMapView *mapView_;
     
     NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
     
+    // If the admin timed out of his session, tell the admin so and tell them to log
+    //   in again.
     if (![jsonArray[0] boolValue]) {
         alert = [[UIAlertView alloc]
                  initWithTitle:@"Error"
@@ -140,8 +130,26 @@ GMSMapView *mapView_;
         return;
     }
     
+    // If user is an admin and an error needs to be fixed, segue to the FixErrorView.
     [self performSegueWithIdentifier:@"FixError" sender:nil];
     return;
+}
+
+#pragma mark - Navigation
+
+// Pass on information to either the MPSErrorViewController or the MPSFixErrorController
+//   before the segue.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.destinationViewController class] == [MPSErrorViewController class]) {
+        MPSErrorViewController *detailController = segue.destinationViewController;
+        detailController.printer = self.printer;
+        detailController.title = [self.printer name];
+    } else {
+        MPSFixErrorController *detailController = segue.destinationViewController;
+        detailController.printer = self.printer;
+        detailController.title = [self.printer name];
+    }
 }
 
 @end
