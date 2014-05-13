cc90a3339265da32d96220c331b3ecaff7c7d220
Michael J Kim <mjkim1993@gmail.com>
2014-05-13 02:39:23 -0400

    comments for AdminView

--- myPrintston/MPSAdminViewController.m
+++ myPrintston/MPSAdminViewController.m
@@ -29,23 +29,26 @@ extern NSString *IP;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
+// Everytime this view appears, immediately redirect to either log in or log out view
+//   based on whether the user is an admin or not.
 - (void) viewDidAppear:(BOOL)animated
 {
     UIAlertView *alert;
     
+    // Direct the user to the log in view if the user is not logged in internally.
     if (!isAdmin) {
         [self performSegueWithIdentifier:@"LogIn" sender:nil];
         return;
     }
     
-
+    // If the admin is logged in internally, make sure the session has not timed out.
     NSString *urlstring = [NSString stringWithFormat:@"%@/checklogin", IP];
     NSURL *url = [NSURL URLWithString:urlstring];
     NSData *data = [NSData dataWithContentsOfURL:url];
     
+    // If there is no connection to the server tell the admin.
     if (!data) {
         isAdmin = NO;
         alert = [[UIAlertView alloc]
@@ -59,6 +62,7 @@ extern NSString *IP;
     
     NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
     
+    // If the admin has timed out, tell the admin and tell the admin to log in again.
     if (![jsonArray[0] boolValue]) {
         isAdmin = NO;
         alert = [[UIAlertView alloc]
@@ -70,6 +74,7 @@ extern NSString *IP;
         return;
     }
     
+    // If the user really is an admin, direct the admin to the log out view.
     [self performSegueWithIdentifier:@"LogOut" sender:nil];
 }
 
